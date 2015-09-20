1bdb580a3e46da45afac52cf513ab92daced2565
Philipp Dikmann <philipp@dikmann.de>
2015-09-20 20:17:03 +0200

    solid base Stream implementation

--- src/Spawner.h
+++ src/Spawner.h
@@ -3,21 +3,14 @@
 class Spawner
 {
 public:
-    double timer;
-    int timerLastFired;
-    float bulletAngle;
-    // IncStream<float> angles;
-    // BulletStream bstream;
-    CountStream cstream;
+    Stream< BulletArray* >* stream;
     Spawner( )
-        : timer( 0.0d )
-        , timerLastFired( 0 )
-        , bulletAngle( 0.0f )
-        //, angles( 0, PI / 2 )
-        //, bstream( new IncStream< float >( 0, PI / 8 ) )
-        , cstream( 4,
-                   new BulletStream(
-                       new IncStream< float >( 0, PI / 12 ) ) )
+        : stream( new IntervalStream(
+              0.2,
+              new TakeStream(
+                  3,
+                  new BulletStream( new IncStream< float >(
+                      0, PI / 4 ) ) ) ) )
     {
     }
     /**
@@ -25,18 +18,7 @@ public:
      */
     BulletArray* update( double deltaTime )
     {
-        BulletArray* b = NULL;
-        timer += deltaTime;
-        float tfloor = floor( timer * 7 );
-        if ( tfloor > timerLastFired )
-        {
-            timerLastFired = tfloor;
-            b = cstream.next( );
-            /* b = new Bullet( */
-            /*     300, 300, angles.next( ),  // bulletAngle, */
-            /*     50.0f + ( ( timerLastFired % 7 ) * 10 ) ); */
-            // bulletAngle += PI / 12;
-        }
+        BulletArray* b = stream->next( deltaTime );
         return b;
     }
 };
--- src/Stream.h
+++ src/Stream.h
@@ -1,22 +1,57 @@
 #pragma once
 
 // composition order:
-// dt ---------------------->|
+// INPUTS:
+// dt -------------------------->|
 // sequence -> composition -> time -> count -> bullet -> angle
 //                                                    -> speed
-//                                    |<------ bullet
-// |<-------------------------------- bulletarray
+//                                    |<------ Bullet*
+// |<-------------------------------- BulletArray*
+// OUTPUTS:
 
+// ???????? how to implement ????????
+// carousel (in1, in2, ..., inn ): each next() switches b/w in1,
+// in2, ..., inn, in1, in2, ...)
+
+// how about: all streams return the same type?
+// e.g. bulletarray?
+// and take the same deltatime-argument (and just don't use it?)
+// then we can mix and match freely?
+
+// can Take/Count be called MultiPoll instead?
+
+
+// -------------------------------------------------------------
+// Most abstract Stream
+// Use this for maximum flexibility
 template < typename T >
 class Stream
 {
 public:
     Stream( ){};
     virtual T next( );
+    virtual T next( double deltaTime );
 };
 
+// -------------------------------------------------------------
+/// Sometimes a Stream doesn't give a damn about `deltaTime`
+/// so here's a default implementation discarding it.
 template < typename T >
-class IncStream : public Stream< T >
+class ItemStream : public Stream< T >
+{
+public:
+    ItemStream( ){};
+    virtual T next( );
+    T next( double deltaTime )
+    {
+        return next( );
+    }
+};
+
+// -------------------------------------------------------------
+/// Increment (and return) a value with each call to `next()`.
+template < typename T >
+class IncStream : public ItemStream< T >
 {
 private:
     T n;
@@ -30,30 +65,38 @@ public:
     {
         return n += increment;
     };
+    T next( double )
+    {
+        return next( );
+    };
 };
 
-class BulletStream : public Stream< Bullet* >
+// -------------------------------------------------------------
+/// Generates single `Bullet*`.
+class BulletStream : public ItemStream< Bullet* >
 {
 private:
     Stream< float >* angles;
 
 public:
-    BulletStream( Stream< float >* ang )
-        : angles( ang ){};
+    BulletStream( Stream< float >* angleStream )
+        : angles( angleStream ){};
     Bullet* next( )
     {
         return new Bullet( 300, 300, angles->next( ), 50 );
     };
 };
 
-class CountStream : public Stream< BulletArray* >
+// -------------------------------------------------------------
+/// Takes multiple `Bullet*` and returns a `BulletArray*`.
+class TakeStream : public ItemStream< BulletArray* >
 {
 private:
     int count;
     Stream< Bullet* >* stream;
 
 public:
-    CountStream( int c, Stream< Bullet* >* from )
+    TakeStream( int c, Stream< Bullet* >* from )
         : count( c )
         , stream( from ){};
     BulletArray* next( )
@@ -67,25 +110,36 @@ public:
     }
 };
 
-/* template < typename T > */
-/* class TimeStream */
-/* { */
-/* public: */
-/*     TimeStream( ){}; */
-/*     virtual T next( double dt ); */
-/* } */
+// -------------------------------------------------------------
+/// Calls `next()` on a given stream in defined intervals (using `deltaTime`).
+class IntervalStream : public Stream< BulletArray* >
+{
+private:
+    double time;
+    int counter;
+    double interval;
+    Stream< BulletArray* >* stream;
 
-/* class IntervalStream : TimeStream< Bullet > */
-/* { */
-/* private: */
-/*     double time; */
-/*     double interval; */
-/*     int counter; */
-
-/* public: */
-/*     IntervalStream( double iv ) */
-/*         : time( 0 ) */
-/*         , interval( iv ) */
-/*         , counter( 0 ){}; */
-/*     Bullet */
-/* }  */
+public:
+    IntervalStream( double ival, Stream< BulletArray* >* from )
+        : time( 0.0d )
+        , counter( 0 )
+        , interval( ival )
+        , stream( from ){};
+    BulletArray* next( )
+    {
+        return NULL;
+    }
+    BulletArray* next( double deltaTime )
+    {
+        BulletArray* b = NULL;
+        time += deltaTime;
+        float tfloor = floor( time / interval );
+        if ( tfloor > counter )
+        {
+            counter = tfloor;
+            b = stream->next( );
+        }
+        return b;
+    }
+};
